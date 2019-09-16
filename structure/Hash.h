4b490d1fee6102c1a5bff250f43383d367e83331
Andy China <xtet2008@126.com>
2019-09-16 22:45:51 +0800

    add OpenHash which using the sequence key slot to store value

--- structure/Hash.h
+++ structure/Hash.h
@@ -11,7 +11,7 @@ typedef struct TagNode{
     struct TagNode *next;
 }*Pointer, Node;
 #define length 20 //定义链表长度
-typedef Pointer LinkHash[length]; //代表着数组 LinkHash[i]中的任意元素i，都是一个 Pointer类型的结构体，并含有 key和next元素(域 )
+typedef Pointer LinkHash[length]; //代表着数组 LinkHash[i]中的任意元素i，都是一个 Pointer指针类型的结构体，并含有 key和next元素(域 )
 
 //计算散列地址
 #define hash_mod 13 //定义散列计算值
@@ -98,4 +98,60 @@ void PrintLinkHash(LinkHash HP){
     }
 
     printf("\nprint for link hash ending ------------------");
+}
+
+
+
+//线性探测法散列表 (如果key存在，则key++，直到后面的元素槽为空为止)
+typedef struct{
+    KeyType key; //键值域
+    //other fields //其他域
+}*Element;
+typedef Element OpenHash[length]; //代表着数组 OpenHash[i]中的任意元素i，都是一个 Element指针类型的结构体，并含有 key元素(域 )
+
+int SearchOpenHash(KeyType key, OpenHash HL)
+// 在散列表HL中查找键值为 key的结点，成功时返回该位置，不成功时返回标志0，假定以线性探测法解决冲突
+{
+    int d = Hash(key); //计算散列地址
+    int i = d;
+    int slot_used = 1;
+    while(slot_used<length && HL[i]!= NULL && HL[i]->key != key){
+        slot_used ++; //用来记录找了多少个槽了，防止槽满了，死循环永无止境的找
+        i = (i+1) % hash_mod; //继续往后面循环找槽（到底后，又会重新从槽的开始位置找）
+    }
+
+    if(HL[i]==NULL || HL[i]->key == key){
+        return i; //查找成功
+    }else{
+        return -1; //查找失败
+    }
+}
+
+//创建散列表 (根据线性探测法，如果key对应的插槽用过了，则往后插槽偏移寻找空余的槽)
+void CreateOpenHash(OpenHash HL, int arr[], int arr_len)
+{
+    for(int i =0; i<length; i++) { HL[i] = NULL;} //初始化HP散列表
+    for(int i = 0; i < arr_len; i++){ 
+        //往散列表里插入数据
+        int slot_index = SearchOpenHash(arr[i], HL);
+        if(slot_index >= 0){
+            Element el = (Element *)malloc(sizeof(Element));
+            el->key = arr[i];
+            HL[slot_index] = el;
+        }
+    }
+}
+
+//打印线性散列表
+void PrintOpenHash(OpenHash HL){
+    printf("\nprint for open hash start ------------------");
+
+    for(int i=0; i<length; i++){
+        Element el = HL[i];
+        if(el != NULL){
+            printf("\nhash key %d: %d", i, el->key);
+        }
+    }
+
+    printf("\nprint for open hash ending ------------------");
 }
--- testing/Hash.c
+++ testing/Hash.c
@@ -2,13 +2,18 @@
 
 void main(){
     int arr[11] = {26, 41, 25, 05, 07, 15, 12, 49, 51, 31, 62};
-    LinkHash HP; //创建散列表
+    LinkHash HP; //创建散列表 (链地址)
     CreateLinkHash(HP, arr, 11);
     PrintLinkHash(HP);
 
     DeleteLinkHash(12, HP);
     printf("\n\nafter the value 12 deleted.");
     PrintLinkHash(HP);
+
+    printf("\n\n");
+    OpenHash HL; //创建散列表 (线性探测法)
+    CreateOpenHash(HL, arr, 11);
+    PrintOpenHash(HL);
 
     getch();
 }
